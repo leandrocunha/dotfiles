a6d9106df46af62ab9c012eafae3cb000c70cee4
Leandro Cunha <leandroscunha@gmail.com>
2023-01-31 21:41:41 +0100

    feat: add neovim bootstrap config

---
+++ nvim/after/plugin/mason.lua
@@ -0,0 +1,6 @@
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+-- After setting up mason-lspconfig you may set up servers via lspconfig
+-- require("lspconfig").sumneko_lua.setup {}
+-- require("lspconfig").rust_analyzer.setup {}
---
+++ nvim/lua/lcunha/init.lua
@@ -0,0 +1,2 @@
+require("lcunha.remap")
+require("lcunha.packer")
