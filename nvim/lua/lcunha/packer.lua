af68e229f52bbce89c68085221e3e9d496011fa8
Leandro Cunha <leandroscunha@gmail.com>
2023-07-12 20:28:04 +0200

    fix: code formatting based on root .editorconfig

--- nvim/lua/lcunha/packer.lua
+++ nvim/lua/lcunha/packer.lua
@@ -1,29 +1,30 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use("wbthomason/packer.nvim")
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.x",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
 
   -- colorscheme
-  use { 'dracula/vim', as = 'dracula' }
-  use { 'catppuccin/nvim', as = 'catppuccin' }
+  use({ "dracula/vim", as = "dracula" })
+  use({ "catppuccin/nvim", as = "catppuccin" })
 
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
   -- lsp
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'lewis6991/gitsigns.nvim'
-  use 'jose-elias-alvarez/null-ls.nvim'
+  use("neovim/nvim-lspconfig")
+  use("williamboman/mason.nvim")
+  use("williamboman/mason-lspconfig.nvim")
+  use("lewis6991/gitsigns.nvim")
+  use("jose-elias-alvarez/null-ls.nvim")
 
   -- lsp UI
   use({
@@ -33,55 +34,55 @@ return require('packer').startup(function(use)
       require("lspsaga").setup({})
     end,
     requires = {
-      {"nvim-tree/nvim-web-devicons"},
+      { "nvim-tree/nvim-web-devicons" },
       --Please make sure you install markdown and markdown_inline parser
-      {"nvim-treesitter/nvim-treesitter"}
-    }
+      { "nvim-treesitter/nvim-treesitter" },
+    },
   })
 
   -- code completition
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-cmdline")
+  use("hrsh7th/nvim-cmp")
 
   -- snippets
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'hrsh7th/cmp-nvim-lua'
+  use("L3MON4D3/LuaSnip")
+  use("saadparwaiz1/cmp_luasnip")
+  use("hrsh7th/cmp-nvim-lua")
 
   -- autopairs and autotag
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
+  use("windwp/nvim-autopairs")
+  use("windwp/nvim-ts-autotag")
 
   -- buffers
-  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+  use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" })
 
   -- identline
-  use 'lukas-reineke/indent-blankline.nvim'
-  use 'tpope/vim-sleuth'
+  use("lukas-reineke/indent-blankline.nvim")
+  use("tpope/vim-sleuth")
 
   -- relative numbers
-  use { "sitiom/nvim-numbertoggle" }
+  use({ "sitiom/nvim-numbertoggle" })
 
   -- comment
-  use 'numToStr/Comment.nvim'
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
+  use("numToStr/Comment.nvim")
+  use("JoosepAlviste/nvim-ts-context-commentstring")
 
   -- lualine
-  use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-}
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "nvim-tree/nvim-web-devicons", opt = true },
+  })
 
   -- vs-tasks
-use {
-  'EthanJWright/vs-tasks.nvim',
-  requires = {
-    'nvim-lua/popup.nvim',
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim'
-  }
-}
+  use({
+    "EthanJWright/vs-tasks.nvim",
+    requires = {
+      "nvim-lua/popup.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  })
 end)
