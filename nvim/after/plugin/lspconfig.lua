52e5403ed2527c23afddf211e918657ef5d6cc45
Leandro Cunha <leandro_cunha@epam.com>
2023-07-26 09:01:58 +0200

    feat: remove lsp diagnostics popup float and back to the inline

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -11,9 +11,9 @@ vim.o.updatetime = 250
 vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 
 -- Disable inline vim diagnostics, using open_float popup
-vim.diagnostic.config({
-  virtual_text = false,
-})
+-- vim.diagnostic.config({
+--   virtual_text = false,
+-- })
 
 -- Diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
