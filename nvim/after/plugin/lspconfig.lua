d5027c9e5a63488ec32f7b626990718fc9626212
Leandro Cunha <leandro_cunha@epam.com>
2023-07-26 09:16:45 +0200

    fix: gitsigns icons for NerdFont

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -16,7 +16,7 @@ vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {
 -- })
 
 -- Diagnostic symbols in the sign column (gutter)
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = "", Hint = "󰌶 ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
