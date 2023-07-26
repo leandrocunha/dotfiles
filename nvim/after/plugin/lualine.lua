f294762a7560013bfd16fd8f85a7f36d2c6be054
Leandro Cunha <leandro_cunha@epam.com>
2023-07-26 14:28:34 +0200

    feat: add folder path to lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,1 +1,12 @@
-require('lualine').setup()
+require('lualine').setup({
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = {{
+      'filename',
+      path = 1
+    }},
+    lualine_c = {
+      'branch', 'diff', 'diagnostics'
+    }
+  }
+})
