eebe7953860d458282a56f37cc6b3e8b64ddcd64
Leandro Cunha <leandroscunha@gmail.com>
2023-08-01 21:22:10 +0200

    fix: update telescope prompt_prefix symbol

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,46 +1,46 @@
-local telescope = require('telescope')
+local telescope = require("telescope")
 
-telescope.setup{
-	defaults = {
-		layout_config = {
-			prompt_position = 'top',
-		},
-		prompt_prefix = '  ',
-		sorting_strategy = 'ascending',
-	},
-	pickers = {
-		find_files = {
-			find_command = {'rg', '--files', '--hidden', '-g', '!.git'},
-			layout_config = {
-				height = 0.70
-			}
-		},
-		buffers = {
-			show_all_buffers = true
-		},
-		live_grep = {
-			previewer = false,
-			theme = "dropdown"
-		},
-		git_status = {
-			git_icons = {
-				added = " ",
-				changed = " ",
-				copied = " ",
-				deleted = " ",
-				renamed = "➡",
-				unmerged = " ",
-				untracked = " ",
-			},
-			previewer = false,
-			theme = "dropdown"
-		}
-	}
-}
+telescope.setup({
+  defaults = {
+    layout_config = {
+      prompt_position = "top",
+    },
+    prompt_prefix = "󰧚 ",
+    sorting_strategy = "ascending",
+  },
+  pickers = {
+    find_files = {
+      find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+      layout_config = {
+        height = 0.70,
+      },
+    },
+    buffers = {
+      show_all_buffers = true,
+    },
+    live_grep = {
+      previewer = false,
+      theme = "dropdown",
+    },
+    git_status = {
+      git_icons = {
+        added = " ",
+        changed = " ",
+        copied = " ",
+        deleted = " ",
+        renamed = "➡",
+        unmerged = " ",
+        untracked = " ",
+      },
+      previewer = false,
+      theme = "dropdown",
+    },
+  },
+})
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", function()
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
