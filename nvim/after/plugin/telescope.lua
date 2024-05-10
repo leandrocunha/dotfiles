0b9ff9202f97ef27cd3b93e53e91e4383341b488
Leandro Cunha <leandroscunha@gmail.com>
2024-05-10 12:57:24 +0200

    feat: add custom picker to show filename first

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,22 @@
 local telescope = require("telescope")
 
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "TelescopeResults",
+	callback = function(ctx)
+		vim.api.nvim_buf_call(ctx.buf, function()
+			vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+			vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+		end)
+	end,
+})
+
+local function filenameFirst(_, path)
+	local tail = vim.fs.basename(path)
+	local parent = vim.fs.dirname(path)
+	if parent == "." then return tail end
+	return string.format("%s\t\t%s", tail, parent)
+end
+
 telescope.setup({
   defaults = {
     layout_config = {
@@ -14,6 +31,7 @@ telescope.setup({
       layout_config = {
         height = 0.70,
       },
+      path_display = filenameFirst,
     },
     buffers = {
       show_all_buffers = true,
