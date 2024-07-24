37ebfa998dbe0e188ea0e358bc07a38091178a53
Leandro Cunha <leandroscunha@gmail.com>
2024-07-24 14:10:37 +0200

    feat: update close buffer shortcut to allow built-in buffer switch

--- nvim/lua/lcunha/remap.lua
+++ nvim/lua/lcunha/remap.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 -- buffers
 vim.keymap.set("n", "<C-h>", vim.cmd.bprevious)
 vim.keymap.set("n", "<C-l>", vim.cmd.bnext)
-vim.keymap.set("n", "<C-w>", vim.cmd.bdelete)
+vim.keymap.set("n", "<C-q>", vim.cmd.bdelete)
 vim.keymap.set("n", "<C-b>", vim.cmd.Ex)
 vim.keymap.set("n", "<C-n>", vim.cmd.enew)
 
