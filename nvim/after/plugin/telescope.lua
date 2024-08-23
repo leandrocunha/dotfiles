bf7b658e794c5e6c389658c81dcc05f28d344220
Leandro Cunha <leandroscunha@gmail.com>
2024-08-23 09:26:29 +0200

    feat: update keybind to search string and avoid format conflict

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -58,7 +58,7 @@ telescope.setup({
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "gr", function()
   builtin.lsp_references({ show_line = false })
